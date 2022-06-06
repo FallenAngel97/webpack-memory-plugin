27796c1639a99233819cd042fbb61c909a19950a
Oleksii Demennikov <odemennikov@lohika.com>
2022-06-06 08:41:46 +0300

    Added the failed hook

--- WebpackMemoryPlugin.js
+++ WebpackMemoryPlugin.js
@@ -1,10 +1,17 @@
 class WebpackMemoryPlugin {
   apply(compiler) {
+    compiler.hooks.failed.tap(
+      'WebpackMemoryPlugin',
+      () => {
+        const used = process.memoryUsage().heapUsed / 1024 / 1024;
+        console.log(`🐏 Amount of RAM used: ${Math.round(used * 100) / 100} MB`);
+      }
+    );
     compiler.hooks.done.tap(
       'WebpackMemoryPlugin',
       () => {
         const used = process.memoryUsage().heapUsed / 1024 / 1024;
-        console.log(`üêè Amount of RAM used: ${Math.round(used * 100) / 100} MB`);
+        console.log(`🐏 Amount of RAM used: ${Math.round(used * 100) / 100} MB`);
       }
     );
   }
